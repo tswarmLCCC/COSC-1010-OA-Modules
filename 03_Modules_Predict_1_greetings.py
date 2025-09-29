373e657b42ed1551306bd71ce0eca890af87e710
Trevor Swarm <tswarm@lccc.wy.edu>
2025-09-29 15:28:31 -0600

    Create 03_Modules_Predict_1_greetings.py

---
+++ 03_Modules_Predict_1_greetings.py
@@ -0,0 +1,14 @@
+"""
+Filename: 03_Modules_Predict_1_greetings.py
+
+This file is a module. It contains functions but is not meant to be run
+directly. It will be imported by `04_Modules_Predict_1_main.py`.
+"""
+
+def say_hello():
+    """A simple function in our module."""
+    print("Hello from the greetings module!")
+
+def say_goodbye():
+    """Another simple function."""
+    print("Goodbye from the greetings module!")
