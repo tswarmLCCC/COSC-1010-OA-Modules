4bfe0b815eb93a013ff240de5ca10c7809fb8488
Trevor Swarm <tswarm@lccc.wy.edu>
2025-09-29 15:24:13 -0600

    Add greetings module with hello and goodbye functions

---
+++ greetings.py
@@ -0,0 +1,7 @@
+def say_hello():
+    """A simple function in our module."""
+    print("Hello from the greetings module!")
+
+def say_goodbye():
+    """Another simple function."""
+    print("Goodbye from the greetings module!")
