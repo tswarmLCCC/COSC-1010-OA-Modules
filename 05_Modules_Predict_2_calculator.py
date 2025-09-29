8d5f7baa0921f7b08d5ed1c5573fea3ded23ded1
Trevor Swarm <tswarm@lccc.wy.edu>
2025-09-29 15:29:09 -0600

    Create calculator module with calculation function
    This module contains a calculation function for use in another script.

---
+++ 05_Modules_Predict_2_calculator.py
@@ -0,0 +1,10 @@
+"""
+Filename: 05_Modules_Predict_2_calculator.py
+
+This file is a module. It contains a function that will be imported by
+`06_Modules_Predict_2_main.py`.
+"""
+
+def do_calculation():
+    """The module's version of the function."""
+    return "This is the result from the 'calculator' module."
