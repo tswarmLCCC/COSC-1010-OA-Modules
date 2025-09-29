444c47ec2955a171ed3ed5ab9c0d0694eb960293
Trevor Swarm <tswarm@lccc.wy.edu>
2025-09-29 15:31:30 -0600

    Create geometry module with area calculation functions
    This file defines functions to calculate the area of various geometric shapes, including square, rectangle, and circle.

---
+++ 12_Modules_Make_Assignment_geometry.py
@@ -0,0 +1,44 @@
+"""
+Filename: 12_Modules_Make_Assignment_geometry.py
+
+The Geometry Calculator (Module File)
+
+This is the first of two files for your assignment. This file should be saved
+as `geometry.py`.
+
+This file will be your module. It should not be run directly.
+Its only job is to define functions that can be used by another script.
+
+Instructions:
+1.  Add the `import math` statement at the top, as you will need it for the
+    circle calculation.
+2.  Complete the function definitions below. Each function should `return` the
+    calculated area. Do not use `print()` statements inside these functions.
+"""
+
+# TODO: Import the math module here.
+
+
+def area_of_square(side):
+    """
+    Calculates the area of a square.
+    Formula: area = side * side
+    """
+    # TODO: Write the code to calculate and return the area.
+    pass # The 'pass' keyword is a placeholder. You should remove it.
+
+def area_of_rectangle(length, width):
+    """
+    Calculates the area of a rectangle.
+    Formula: area = length * width
+    """
+    # TODO: Write the code to calculate and return the area.
+    pass
+
+def area_of_circle(radius):
+    """
+    Calculates the area of a circle.
+    Formula: area = pi * radius^2
+    """
+    # TODO: Write the code to calculate and return the area.
+    pass
